5dd73637f7cca9d152a1d67d2a6b81456f0dc0f0
xushanchuan <aisensiy@163.com>
2016-08-01 16:11:39 +0800

    use port env

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import config
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,4 @@ def hello():
     return "Hello, world!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=config.PORT)
